4b18e047fc19a4da36460e4400a56f1ec28ef849
jlloaiza <jlloaiza04@gmail.com>
2022-02-28 01:32:11 -0500

    first functional commit

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,14 @@
+import { db } from '../database/connection';
+import { collection, query, where, getDoc, getDocs, doc } from 'firebase/firestore';
+
+export async function getUsers(req, res, id) {
+    const docRef = doc(db, "users", "6ClOZsipScOpDMG8GzdW");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}
