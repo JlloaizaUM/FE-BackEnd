a3a316e94fee5894dd636e5546c788a2a5d9748c
Jorge Loaiza <jlloaiza04@gmail.com>
2022-03-27 19:01:58 -0500

    hotfix #2

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.set('port', config.port);
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', "https://bonapettit.com.co/");
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, *');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
